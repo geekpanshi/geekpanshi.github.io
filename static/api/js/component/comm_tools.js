5ea762b08f6f249a7640fe2710af755c0a374526
shshixingang <shshixingang@corp.netease.com>
2018-10-22 17:47:04 +0800

    update

---
+++ static/api/js/component/comm_tools.js
@@ -0,0 +1,2 @@
+// build time:Mon Oct 22 2018 17:45:05 GMT+0800 (中国标准时间)
+!window._bd_share_is_recently_loaded&&window._bd_share_main.F.module("component/comm_tools",function(n,o){var r=function(){var n=window.location||document.location||{};return n.href||""},t=function(n,o){var r=n.length,t="";for(var a=1;a
