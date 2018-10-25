d484d0e0c09a3b443734f75e742f4a5303b90643
shshixingang <shshixingang@corp.netease.com>
2018-10-25 12:35:09 +0800

    update

--- static/api/js/share/image_api.js
+++ static/api/js/share/image_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 25 2018 08:01:42 GMT+0800 (中国标准时间)
+// build time:Thu Oct 25 2018 09:26:49 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/image_api",function(e,i,a){var n=e("base/tangram").T,t=e("base/class").Class,r=e("component/comm_tools"),o=e("share/api_base");i.Api=t.create(function(e){var i=this;i._init=function(){var e=i.getView();e.render(),e.init(),e.on("moreover",function(){e._keepBarVisible()})},i._processAction=function(a){var n=i.getView();return e.bdPic=n._getImageSrc(),{data:{type:"imgshare"}}},i._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 25 2018 08:01:42 GMT+0800 (中国标准时间)
+// build time:Thu Oct 25 2018 09:26:49 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
