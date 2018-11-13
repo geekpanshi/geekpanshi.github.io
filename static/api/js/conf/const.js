e75dad04a27dc9ff29e3b750edea8ec7e824c978
shshixingang <shshixingang@corp.netease.com>
2018-11-13 11:19:40 +0800

    update 20181113

--- static/api/js/conf/const.js
+++ static/api/js/conf/const.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 12 2018 13:03:38 GMT+0800 (中国标准时间)
+// build time:Tue Nov 13 2018 11:18:19 GMT+0800 (中国标准时间)
 !window._bd_share_is_recently_loaded&&window._bd_share_main.F.module("conf/const",function(t,a,c){a.CMD_ATTR="data-cmd",a.CONFIG_TAG_ATTR="data-tag",a.URLS={likeSetUrl:"http://like.baidu.com/set",commitUrl:"http://s.share.baidu.com/commit",jumpUrl:"http://s.share.baidu.com",mshareUrl:"http://s.share.baidu.com/mshare",emailUrl:"http://s.share.baidu.com/sendmail",nsClick:"http://nsclick.baidu.com/v.gif",backUrl:"http://s.share.baidu.com/back",shortUrl:"http://dwz.cn/v2cut.php"}});
 //rebuild by neat 
