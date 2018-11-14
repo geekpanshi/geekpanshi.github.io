fe26fec19d7fd40cc43871f025d3de6269a437a5
shshixingang <shshixingang@corp.netease.com>
2018-11-14 10:12:59 +0800

    update tag color

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Wed Nov 14 2018 09:49:37 GMT+0800 (中国标准时间)
+// build time:Wed Nov 14 2018 10:10:49 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Wed Nov 14 2018 09:49:37 GMT+0800 (中国标准时间)
+// build time:Wed Nov 14 2018 10:10:49 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
