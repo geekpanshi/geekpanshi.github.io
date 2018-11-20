7103951ed6519d676b297c908172aba0a7df38ef
shshixingang <shshixingang@corp.netease.com>
2018-11-20 10:53:09 +0800

    update 20181120-6

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 20 2018 10:34:36 GMT+0800 (中国标准时间)
+// build time:Tue Nov 20 2018 10:51:41 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 20 2018 10:34:38 GMT+0800 (中国标准时间)
+// build time:Tue Nov 20 2018 10:51:43 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
