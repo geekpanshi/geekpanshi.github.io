f31286ee52373f02820cf8df3c0422e324ef489d
shshixingang <shshixingang@corp.netease.com>
2018-11-22 17:20:30 +0800

    update 20181122-7

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 22 2018 15:23:28 GMT+0800 (中国标准时间)
+// build time:Thu Nov 22 2018 17:18:59 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
