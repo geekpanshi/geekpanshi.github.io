f568a3cf8e95b1a8e9c31da132e5166592beaca8
shshixingang <shshixingang@corp.netease.com>
2018-11-26 13:43:21 +0800

    update 20181126-6

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 26 2018 11:11:59 GMT+0800 (中国标准时间)
+// build time:Mon Nov 26 2018 13:41:34 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
