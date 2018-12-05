9b4b2323cb405bb76535116cdf19ef65bfe771e8
shshixingang <shshixingang@corp.netease.com>
2018-12-05 14:09:54 +0800

    update 20181205-2

--- static/api/js/share/slide_api.js
+++ static/api/js/share/slide_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 05 2018 06:51:00 GMT+0800 (中国标准时间)
+// build time:Wed Dec 05 2018 13:59:23 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/slide_api",function(e,i,t){var n=e("base/tangram").T,a=e("base/class").Class,o=e("share/api_base");i.Api=a.create(function(e){var i=this;i._init=function(){var e=i.getView();e.render(),e.init()},i._slidePop=function(i,t){i._popupBox=t.boxEle,i._getPosition=function(){return{top:0,left:e.bdPos=="left"?0:t.element.width()}},i.show(t,e)},i._processAction=function(e){return{data:{type:"share"}}},i._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 05 2018 06:50:59 GMT+0800 (中国标准时间)
+// build time:Wed Dec 05 2018 13:59:23 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
