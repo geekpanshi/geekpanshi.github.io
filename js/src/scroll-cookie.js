69c40b7f1a944a8fd3ad35defa03ef4e5f503261
shshixingang <shshixingang@corp.netease.com>
2018-12-08 07:31:28 +0800

    update 20181208-1

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 07 2018 18:04:05 GMT+0800 (中国标准时间)
+// build time:Sat Dec 08 2018 07:25:50 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 07 2018 18:04:06 GMT+0800 (中国标准时间)
+// build time:Sat Dec 08 2018 07:25:51 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
