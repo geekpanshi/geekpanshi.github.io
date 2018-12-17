db481c2c69a1341e53384152749bb3ffb998c028
shshixingang <shshixingang@corp.netease.com>
2018-12-18 05:42:22 +0800

    update 20181218-1

--- static/api/js/share/select_api.js
+++ static/api/js/share/select_api.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 17 2018 14:06:38 GMT+0800 (中国标准时间)
+// build time:Tue Dec 18 2018 05:40:25 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/select_api",function(e,a,n){var t=e("base/tangram").T,i=e("base/class").Class,s=e("component/comm_tools"),o=e("share/api_base");a.Api=i.create(function(e){var a=this;a._init=function(){var e=a.getView();e.render(),e.init()},a._processAction=function(e){return{data:{type:"select"}}},a._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
