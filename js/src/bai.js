8c9b9b9c450415b9192376015b426c133a8d7195
shshixingang <shshixingang@corp.netease.com>
2018-12-19 07:01:33 +0800

    update 20181219-1

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 18 2018 05:58:12 GMT+0800 (中国标准时间)
+// build time:Wed Dec 19 2018 06:59:00 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
