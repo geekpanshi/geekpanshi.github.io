00d52c3c2968ac198b2f0923357d88407fc4818c
shshixingang <shshixingang@corp.netease.com>
2018-12-19 13:38:45 +0800

    update 20181219-4

--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 19 2018 09:38:46 GMT+0800 (中国标准时间)
+// build time:Wed Dec 19 2018 13:36:40 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
--- static/api/js/trans/trans_bdysc.js
+++ static/api/js/trans/trans_bdysc.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 19 2018 09:38:46 GMT+0800 (中国标准时间)
+// build time:Wed Dec 19 2018 13:36:41 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("trans/trans_bdysc",function(a,n){var e=function(a){var n={url:a.url,title:a.title};if(window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck)window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck.go(n);else{window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck={callback:function(){this.go(n)}};var e=document.createElement("script"),i="http://s.wenzhang.baidu.com/js/pjt/content_ex/page/";i+="bookmark.js?s=baidu_fenxiang&_t="+Math.random(),e.src=i,document.getElementsByTagName("body")[0].appendChild(e)}};n.run=e});
 //rebuild by neat 
