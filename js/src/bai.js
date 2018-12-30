4ab94c7a1a1a47d019853ffffe6a38044877ea1c
vincent <shix_112@126.com>
2018-12-31 02:16:27 +0800

    update 20181231-1

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Sun Dec 30 2018 09:01:49 GMT+0800 (GMT+08:00)
+// build time:Mon Dec 31 2018 02:14:32 GMT+0800 (GMT+08:00)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
