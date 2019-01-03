b82df762a5995457cf372d048ff501cdf9ed066e
vincent <shix_112@126.com>
2019-01-04 07:28:36 +0800

    udpate 20190104-2

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 04 2019 07:21:42 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 04 2019 07:27:38 GMT+0800 (GMT+08:00)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 04 2019 07:21:43 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 04 2019 07:27:39 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
