79529ff1466a170369da6aec02f406e850e6dc3f
vincent <shix_112@126.com>
2019-01-05 08:15:52 +0800

    udpate 20190104-2

---
+++ js/src/bai.js
@@ -0,0 +1,3 @@
+// build time:Sat Jan 05 2019 08:10:45 GMT+0800 (GMT+08:00)
+(function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+//rebuild by neat 
---
+++ static/api/js/trans/logger.js
@@ -0,0 +1,3 @@
+// build time:Sat Jan 05 2019 08:10:45 GMT+0800 (GMT+08:00)
+
+//rebuild by neat 
