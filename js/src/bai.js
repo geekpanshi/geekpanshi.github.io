ed55171d90dc746d6ee53d4a4936a1b01e1100bc
s1635 <shshixingang@corp.netease.com>
2019-01-25 10:23:51 +0800

    update 20190125-1

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 24 2019 16:49:17 GMT+0800 (中国标准时间)
+// build time:Fri Jan 25 2019 10:21:21 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 24 2019 16:49:18 GMT+0800 (中国标准时间)
+// build time:Fri Jan 25 2019 10:21:22 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
