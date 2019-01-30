15d189805112140d1e3bd8fbb15b0ca9a8cbba52
s1635 <shshixingang@corp.netease.com>
2019-01-30 09:49:21 +0800

    update 20190130-1

--- static/api/js/trans/trans_bdxc.js
+++ static/api/js/trans/trans_bdxc.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 29 2019 09:17:22 GMT+0800 (中国标准时间)
+// build time:Wed Jan 30 2019 09:46:23 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("trans/trans_bdxc",function(e,t){var n=function(){var e=window,t=document,n="_bdXC",a;e[n]?window._bdXC_loaded&&e[n].reInit():(a=t.createElement("script"),a.setAttribute("charset","utf-8"),a.src="http://xiangce.baidu.com/zt/collect/mark.js?"+(new Date).getTime(),t.getElementsByTagName("head")[0].appendChild(a))};t.run=n});
 //rebuild by neat 
