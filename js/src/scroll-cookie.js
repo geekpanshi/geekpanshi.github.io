0fd6d21eafe4afb52c476aab2f959bafa172056d
s1635 <shshixingang@corp.netease.com>
2019-03-15 09:15:11 +0800

    update 20190315-1

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 14 2019 13:01:18 GMT+0800 (中国标准时间)
+// build time:Fri Mar 15 2019 09:12:19 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 14 2019 13:01:19 GMT+0800 (中国标准时间)
+// build time:Fri Mar 15 2019 09:12:20 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
