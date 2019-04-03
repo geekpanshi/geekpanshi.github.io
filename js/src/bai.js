29779f9ea5301125dbdc999daaff6ac81e0fac3b
s1635 <shshixingang@corp.netease.com>
2019-04-03 10:36:10 +0800

    update 20190402-2

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 03 2019 09:27:06 GMT+0800 (中国标准时间)
+// build time:Wed Apr 03 2019 10:33:46 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 03 2019 09:27:07 GMT+0800 (中国标准时间)
+// build time:Wed Apr 03 2019 10:33:48 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
