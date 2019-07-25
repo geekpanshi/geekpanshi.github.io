903d66dc2d9e3dad316929a8fdc2c54fa9c8889b
s1635 <shshixingang@corp.netease.com>
2019-07-25 19:25:48 +0800

    注脚使用本地文件

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Thu Jul 25 2019 19:11:45 GMT+0800 (中国标准时间)
+// build time:Thu Jul 25 2019 19:22:58 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Thu Jul 25 2019 19:11:46 GMT+0800 (中国标准时间)
+// build time:Thu Jul 25 2019 19:22:59 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
