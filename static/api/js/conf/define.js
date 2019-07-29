8f85a972aed2914cfd3c7fa1cec953a3044affdf
s1635 <shshixingang@corp.netease.com>
2019-07-29 18:55:14 +0800

    博客所使用的图片全部迁移到 github 上，不再使用以下图床：1. imgur（国外的，国内会出现被墙） 2. 新浪图床（已经不允许第三方调用）

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 29 2019 11:06:33 GMT+0800 (中国标准时间)
+// build time:Mon Jul 29 2019 18:48:03 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 29 2019 11:06:33 GMT+0800 (中国标准时间)
+// build time:Mon Jul 29 2019 18:48:03 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
