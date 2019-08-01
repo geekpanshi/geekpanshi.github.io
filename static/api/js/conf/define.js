c6881fa2bbc3b519615faef67b8d9c656f8eb4ad
s1635 <shshixingang@corp.netease.com>
2019-08-01 14:18:12 +0800

    update : Git、C++、Python、电子书和教程资料等集合

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 01 2019 14:00:57 GMT+0800 (中国标准时间)
+// build time:Thu Aug 01 2019 14:07:55 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 01 2019 14:00:57 GMT+0800 (中国标准时间)
+// build time:Thu Aug 01 2019 14:07:55 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
