52bb36bc915788bd310990477c8080ed03be4050
s1635 <shshixingang@corp.netease.com>
2019-08-04 11:43:42 +0800

    auto_commit by date of 2019年08月 4日 11:43:29

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 11:07:13 GMT+0800 (中国标准时间)
+// build time:Sun Aug 04 2019 11:39:40 GMT+0800 (中国标准时间)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 11:07:13 GMT+0800 (中国标准时间)
+// build time:Sun Aug 04 2019 11:39:40 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
