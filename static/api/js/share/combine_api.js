cbd2fe9ec2270000b7ede9665f8b543a93e8f1ff
s1635 <shshixingang@corp.netease.com>
2019-08-09 13:19:57 +0800

    auto_commit by date of 2019年08月 9日 13:19:42

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 08 2019 09:28:50 GMT+0800 (中国标准时间)
+// build time:Fri Aug 09 2019 13:17:10 GMT+0800 (中国标准时间)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 08 2019 09:28:50 GMT+0800 (中国标准时间)
+// build time:Fri Aug 09 2019 13:17:10 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
