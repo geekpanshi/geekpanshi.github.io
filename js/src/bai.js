2d41e0d82781e9677a4f0bb9d6d25304f9a312ea
s1635 <shshixingang@corp.netease.com>
2019-08-12 19:33:29 +0800

    auto_commit by date of 2019年08月12日 19:33:16

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Mon Aug 12 2019 06:47:27 GMT+0800 (中国标准时间)
+// build time:Mon Aug 12 2019 19:30:54 GMT+0800 (中国标准时间)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
