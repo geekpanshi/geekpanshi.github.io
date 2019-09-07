0c94bc7fa71d0a6ed550389df336f4f03ea2b35b
shshixingang <shshixingang@corp.netease.com>
2019-09-07 21:47:13 +0800

    auto_commit by date of 2019年09月 7日 21:46:59

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Fri Sep 06 2019 09:20:27 GMT+0800 (GMT+08:00)
+// build time:Sat Sep 07 2019 21:45:01 GMT+0800 (GMT+08:00)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Fri Sep 06 2019 09:20:27 GMT+0800 (GMT+08:00)
+// build time:Sat Sep 07 2019 21:45:01 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
