4f06413dea7dc1ad9ff917f1ddf17b41773f1f27
shshixingang <shshixingang@corp.netease.com>
2019-09-17 09:19:40 +0800

    auto_commit by date of 2019年09月17日  9:19:24

--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 16 2019 09:47:07 GMT+0800 (GMT+08:00)
+// build time:Tue Sep 17 2019 09:17:13 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- static/api/js/trans/trans_bdxc.js
+++ static/api/js/trans/trans_bdxc.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 16 2019 09:47:07 GMT+0800 (GMT+08:00)
+// build time:Tue Sep 17 2019 09:17:13 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("trans/trans_bdxc",function(e,t){var n=function(){var e=window,t=document,n="_bdXC",a;e[n]?window._bdXC_loaded&&e[n].reInit():(a=t.createElement("script"),a.setAttribute("charset","utf-8"),a.src="http://xiangce.baidu.com/zt/collect/mark.js?"+(new Date).getTime(),t.getElementsByTagName("head")[0].appendChild(a))};t.run=n});
 //rebuild by neat 
