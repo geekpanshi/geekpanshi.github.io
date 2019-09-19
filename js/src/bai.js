c7e84645c6ad39cc1ed5dc272e70bbbc57695754
shshixingang <shshixingang@corp.netease.com>
2019-09-19 09:41:28 +0800

    auto_commit by date of 2019年09月19日  9:41:11

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Wed Sep 18 2019 09:38:28 GMT+0800 (GMT+08:00)
+// build time:Thu Sep 19 2019 09:39:11 GMT+0800 (GMT+08:00)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/trans/trans_bdysc.js
+++ static/api/js/trans/trans_bdysc.js
@@ -1,3 +1,3 @@
-// build time:Wed Sep 18 2019 09:38:28 GMT+0800 (GMT+08:00)
+// build time:Thu Sep 19 2019 09:39:11 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("trans/trans_bdysc",function(a,n){var e=function(a){var n={url:a.url,title:a.title};if(window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck)window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck.go(n);else{window.baiduSC_yaq4d3elabjnvmijccc1zuo3o4yeizck={callback:function(){this.go(n)}};var e=document.createElement("script"),i="http://s.wenzhang.baidu.com/js/pjt/content_ex/page/";i+="bookmark.js?s=baidu_fenxiang&_t="+Math.random(),e.src=i,document.getElementsByTagName("body")[0].appendChild(e)}};n.run=e});
 //rebuild by neat 
