d6082d132917ad96b7ca57925f6b1d05fccd51fa
shshixingang <shshixingang@corp.netease.com>
2019-12-25 12:38:31 +0800

    auto_commit by date of 2019年12月25日 12:38:14

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 25 2019 10:59:53 GMT+0800 (GMT+08:00)
+// build time:Wed Dec 25 2019 12:35:54 GMT+0800 (GMT+08:00)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
