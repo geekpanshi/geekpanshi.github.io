5f6a3bacd4cfe5352cb7a783591ee7b6646d63a2
shixingang <shshixingang@corp.netease.com>
2020-09-25 15:06:38 +0800

    auto_commit by date of 2020年09月25日 15:06:24

--- static/api/js/share/image_api.js
+++ static/api/js/share/image_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Sep 10 2020 17:27:44 GMT+0800 (GMT+08:00)
+// build time:Fri Sep 25 2020 15:04:12 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/image_api",function(e,i,a){var n=e("base/tangram").T,t=e("base/class").Class,r=e("component/comm_tools"),o=e("share/api_base");i.Api=t.create(function(e){var i=this;i._init=function(){var e=i.getView();e.render(),e.init(),e.on("moreover",function(){e._keepBarVisible()})},i._processAction=function(a){var n=i.getView();return e.bdPic=n._getImageSrc(),{data:{type:"imgshare"}}},i._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
