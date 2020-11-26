1fcc3012e980731b1410a04c9f0343f8c919720a
shixingang <shshixingang@corp.netease.com>
2020-11-26 17:15:52 +0800

    auto_commit by date of 2020年11月26日 17:15:41

--- js/src/bai.js
+++ js/src/bai.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 26 2020 17:00:35 GMT+0800 (GMT+08:00)
+// build time:Thu Nov 26 2020 17:14:07 GMT+0800 (GMT+08:00)
 (function(){var e=document.createElement("script");e.src="//push.zhanzhang.baidu.com/push.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 26 2020 17:00:36 GMT+0800 (GMT+08:00)
+// build time:Thu Nov 26 2020 17:14:09 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
